6107625a938ad9c61ea2aa414f5bbc261875cba7
Li Keqing <me@kaze.ai>
2023-11-16 18:30:39 +0800

    first commit

---
+++ PBRUSH/main.c
@@ -0,0 +1,40 @@
+#include <windows.h>
+
+extern char** _imp___acmdln;
+
+void __cdecl Entry()
+{
+	LPSTR lpCmdLine = GetCommandLineA();
+	if (*lpCmdLine == '\"')
+	{
+		do
+		{
+			lpCmdLine++;
+			if (*lpCmdLine == '\0') break;
+		} while (*lpCmdLine != '\"');
+		if (*lpCmdLine == '\"')
+		{
+			lpCmdLine++;
+		}
+	}
+	else
+	{
+		while (*lpCmdLine > ' ')
+		{
+			lpCmdLine++;
+		}
+	}
+	while ((*lpCmdLine != '\0') && (*lpCmdLine < '!'))
+	{
+		lpCmdLine++;
+	}
+	STARTUPINFOA info;
+	info.dwFlags = 0;
+	GetStartupInfoA(&info);
+	if (!(info.dwFlags & STARTF_USESHOWWINDOW))
+	{
+		info.wShowWindow = SW_SHOWNORMAL;
+	}
+	ShellExecuteA(NULL, NULL, "mspaint.exe", lpCmdLine, NULL, info.wShowWindow);
+	ExitProcess(0);
+}
